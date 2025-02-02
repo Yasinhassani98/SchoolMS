c35750fae22ecc94661610e3274a8b74f972b764
Omar Dyab <omarabdulah2004@gmail.com>
2025-02-03 01:59:49 +0300

    The first commit

---
+++ database/migrations/2025_02_02_224147_create_levels_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('levels', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->enum('level',[1,2,3,4,5,6,7,8,9,10,11,12])->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('levels');
+    }
+};
