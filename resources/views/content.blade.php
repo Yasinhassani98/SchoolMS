54c811520c0b56ee74548f5aa62528f5c8984524
Omar Dyab <omarabdulah2004@gmail.com>
2025-02-09 19:02:36 +0300

    Template Front

---
+++ resources/views/content.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.base')
+@section('content')
+    <div class="content-body">
+        <div class="container-fluid">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, accusamus. Ad, fugit hic sit beatae
+                aspernatur ducimus ea placeat. Possimus neque, nisi aliquam perspiciatis cum voluptas est nihil! Quidem,
+                veritatis.</p>
+        </div>
+    </div>
+@endsection
